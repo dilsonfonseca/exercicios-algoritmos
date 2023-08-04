4155baa90173c4e46899c00b70871bbdd3c03981
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-08-04 20:17:26 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let n1 = parseInt(prompt("Digite o primeiro número"));
+let n2 = parseInt(prompt("Digite o segundo número"));
+
+if (n1 === n2) {
+  console.log("Os números são iguais");
+} else {
+  console.log("Que pena, os números não são iguais");
+}
