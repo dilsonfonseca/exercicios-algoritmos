4c887f68f0511b7515b51989d3c04529da5cbdf5
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-08-04 21:09:53 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-let n1 = parseInt(prompt("Digite o primeiro número"));
-let n2 = parseInt(prompt("Digite o segundo número"));
-
-if (n1 === n2) {
-  console.log("Os números são iguais");
-} else {
-  console.log("Que pena, os números não são iguais");
-}
